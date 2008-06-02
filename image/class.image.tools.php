ffa6359d67e49ef90135a5c59a2d2d3eed410633
olivier@edc8846e-aa25-0410-8aa3-9560d1ba59f0 <olivier@edc8846e-aa25-0410-8aa3-9560d1ba59f0>
2008-06-02 12:45:13 +0000

    In imageTools::memoryAllocate, multiply by 8 instead of 5

--- image/class.image.tools.php
+++ image/class.image.tools.php
@@ -97,7 +97,7 @@ class imageTools
 		{
 			$mem_limit = files::str2bytes($mem_limit);
 			$mem_avail = $mem_limit-$mem_used-(512*1024);
-			$mem_needed = $w*$h*5;
+			$mem_needed = $w*$h*8;
 			
 			if ($mem_needed > $mem_avail)
 			{
