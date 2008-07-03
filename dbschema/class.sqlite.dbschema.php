1b8496a13520ae86c030bdfe9f683fc762498834
olivier@edc8846e-aa25-0410-8aa3-9560d1ba59f0 <olivier@edc8846e-aa25-0410-8aa3-9560d1ba59f0>
2008-07-03 19:12:19 +0000

    SQLite with PDO and SQLite 3 only.

--- dbschema/class.sqlite.dbschema.php
+++ dbschema/class.sqlite.dbschema.php
@@ -24,12 +24,37 @@ class sqliteSchema extends dbSchema implements i_dbSchema
 {
 	private $table_hist = array();
 	
-	private $table_stack = array();
-	private $x_stack = array();
+	private $table_stack = array();	// Stack for tables creation
+	private $x_stack = array();		// Execution stack
 	
 	public function dbt2udt($type,&$len,&$default)
 	{
 		$type = parent::dbt2udt($type,$len,$default);
+		
+		switch ($type)
+		{
+			case 'float':
+				return 'real';
+			case 'double':
+				return 'float';
+			case 'timestamp':
+				# DATETIME real type is TIMESTAMP
+				if ($default == "'1970-01-01 00:00:00'") {
+					# Bad hack
+					$default = 'now()';
+				}
+				return 'timestamp';
+			case 'integer':
+			case 'mediumint':
+			case 'bigint':
+			case 'tinyint':
+			case 'smallint':
+			case 'numeric':
+				return 'integer';
+			case 'tinytext':
+			case 'longtext':
+				return 'text';
+		}
 		
 		return $type;
 	}
@@ -40,11 +65,13 @@ class sqliteSchema extends dbSchema implements i_dbSchema
 		
 		switch ($type)
 		{
+			case 'integer':
 			case 'smallint':
 			case 'bigint':
 				return 'integer';
 			case 'real':
-				return 'float';
+			case 'float:':
+				return 'real';
 			case 'date':
 			case 'time':
 				return 'timestamp';
@@ -61,8 +88,6 @@ class sqliteSchema extends dbSchema implements i_dbSchema
 	
 	public function flushStack()
 	{
-		# Vilain hack of #@! SQLite that doesn't support
-		# ALTER ... ADD CONSTRAINT on tables
 		foreach ($this->table_stack as $table => $def)
 		{
 			$sql = 'CREATE TABLE '.$table." (\n".implode(",\n",$def)."\n)\n ";
@@ -73,33 +98,18 @@ class sqliteSchema extends dbSchema implements i_dbSchema
 		{
 			$this->con->execute($x);
 		}
+		
 	}
 	
 	public function db_get_tables()
 	{
 		$res = array();
-		$sql = "
-		Select * from
-		( select 'main' as TABLE_CATALOG , 'sqlite' as TABLE_SCHEMA ,
-		tbl_name as TABLE_NAME , case when type = 'table'
-		then 'BASE TABLE' when type = 'view' then 'VIEW'
-		end as TABLE_TYPE, sql as TABLE_SOURCE from sqlite_master
-		where type in('table','view')
-		and tbl_name not like 'INFORMATION_SCHEMA_%'
-		union select 'main' as TABLE_CATALOG , 'sqlite' as TABLE_SCHEMA,
-		tbl_name as TABLE_NAME , case when type = 'table'
-		then 'TEMPORARY TABLE' when type = 'view'
-		then 'TEMPORARY VIEW' end as TABLE_TYPE, sql as
-		TABLE_SOURCE from sqlite_temp_master where type in('table','view')
-		and tbl_name not like 'INFORMATION_SCHEMA_%' )
-		BT order by TABLE_TYPE , TABLE_NAME
-		";
-		
+		$sql = "SELECT * FROM sqlite_master WHERE type = 'table'";
 		$rs = $this->con->select($sql);
 		
 		$res = array();
 		while ($rs->fetch()) {
-			$res[] = $rs->TABLE_NAME;
+			$res[] = $rs->tbl_name;
 		}
 		
 		return $res;
@@ -107,26 +117,187 @@ class sqliteSchema extends dbSchema implements i_dbSchema
 	
 	public function db_get_columns($table)
 	{
-		# We may need to do it one day
-		return array();
+		$sql = 'PRAGMA table_info('.$this->con->escapeSystem($table).')';
+		$rs = $this->con->select($sql);
+		
+		$res = array();
+		while ($rs->fetch())
+		{
+			$field = trim($rs->name);
+			$type = trim($rs->type);
+			$null = trim($rs->notnull) == 0;
+			$default = trim($rs->dflt_value);
+			
+			$len = null;
+			if (preg_match('/^(.+?)\(([\d,]+)\)$/si',$type,$m)) {
+				$type = $m[1];
+				$len = (integer) $m[2];
+			}
+			
+			$res[$field] = array(
+				'type' => $type,
+				'len' => $len,
+				'null' => $null,
+				'default' => $default
+			);
+		}
+		return $res;
 	}
 	
 	public function db_get_keys($table)
 	{
-		# Don't need this with SQLite
-		return array();
+		$t = array();
+		$res = array();
+		
+		# Get primary keys first
+		$sql = "SELECT sql FROM sqlite_master WHERE type='table' AND name='".$this->con->escape($table)."'";
+		$rs = $this->con->select($sql);
+		
+		if ($rs->isEmpty()) {
+			return array();
+		}
+		
+		# Get primary keys
+		$n = preg_match_all('/^\s*CONSTRAINT\s+([^,]+?)\s+PRIMARY\s+KEY\s+\((.+?)\)/msi',$rs->sql,$match);
+		if ($n > 0)
+		{
+			foreach ($match[1] as $i => $name)
+			{
+				$cols = preg_split('/\s*,\s*/',$match[2][$i]);
+				$res[] = array(
+					'name' => $name,
+					'primary' => true,
+					'unique' => false,
+					'cols' => $cols
+				);
+			}
+		}
+		
+		# Get unique keys
+		$n = preg_match_all('/^\s*CONSTRAINT\s+([^,]+?)\s+UNIQUE\s+\((.+?)\)/msi',$rs->sql,$match);
+		if ($n > 0)
+		{
+			foreach ($match[1] as $i => $name)
+			{
+				$cols = preg_split('/\s*,\s*/',$match[2][$i]);
+				$res[] = array(
+					'name' => $name,
+					'primary' => false,
+					'unique' => true,
+					'cols' => $cols
+				);
+			}
+		}
+		
+		return $res;
 	}
 	
 	public function db_get_indexes($table)
 	{
-		# Don't need this with SQLite
-		return array();
+		$sql = 'PRAGMA index_list('.$this->con->escapeSystem($table).')';
+		$rs = $this->con->select($sql);
+		
+		$res = array();
+		while ($rs->fetch())
+		{
+			if (preg_match('/^sqlite_/',$rs->name)) {
+				continue;
+			}
+			
+			$idx = $this->con->select('PRAGMA index_info('.$this->con->escapeSystem($rs->name).')');
+			$cols = array();
+			while ($idx->fetch()) {
+				$cols[] = $idx->name;
+			}
+			
+			$res[] = array(
+				'name' => $rs->name,
+				'type' => 'btree',
+				'cols' => $cols
+			);
+		}
+		
+		return $res;
 	}
 	
 	public function db_get_references($table)
 	{
-		# Don't need this with SQLite
-		return array();
+		$sql = 'SELECT * FROM sqlite_master WHERE type=\'trigger\' AND tbl_name = \'%1$s\' AND name LIKE \'%2$s_%%\' ';
+		$res = array();
+		
+		# Find constraints on table
+		$bir = $this->con->select(sprintf($sql,$this->con->escape($table),'bir'));
+		$bur = $this->con->select(sprintf($sql,$this->con->escape($table),'bur'));
+		
+		if ($bir->isEmpty() || $bur->isempty()) {
+			return $res;
+		}
+		
+		while ($bir->fetch())
+		{
+			# Find child column and parent table and column
+			if (!preg_match('/FROM\s+(.+?)\s+WHERE\s+(.+?)\s+=\s+NEW\.(.+?)\s*?\) IS\s+NULL/msi',$bir->sql,$m)) {
+				continue;
+			}
+			
+			$c_col = $m[3];
+			$p_table = $m[1];
+			$p_col = $m[2];
+			
+			# Find on update
+			$on_update = 'restrict';
+			$aur = $this->con->select(sprintf($sql,$this->con->escape($p_table),'aur'));
+			while ($aur->fetch())
+			{
+				if (!preg_match('/AFTER\s+UPDATE/msi',$aur->sql)) {
+					continue;
+				}
+				
+				if (preg_match('/UPDATE\s+'.$table.'\s+SET\s+'.$c_col.'\s*=\s*NEW.'.$p_col.
+					'\s+WHERE\s+'.$c_col.'\s*=\s*OLD\.'.$p_col.'/msi',$aur->sql)) {
+					$on_update = 'cascade';
+					break;
+				}
+				
+				if (preg_match('/UPDATE\s+'.$table.'\s+SET\s+'.$c_col.'\s*=\s*NULL'.
+					'\s+WHERE\s+'.$c_col.'\s*=\s*OLD\.'.$p_col.'/msi',$aur->sql)) {
+					$on_update = 'set null';
+					break;
+				}
+			}
+			
+			# Find on delete
+			$on_delete = 'restrict';
+			$bdr = $this->con->select(sprintf($sql,$this->con->escape($p_table),'bdr'));
+			while ($bdr->fetch())
+			{
+				if (!preg_match('/BEFORE\s+DELETE/msi',$bdr->sql)) {
+					continue;
+				}
+				
+				if (preg_match('/DELETE\s+FROM\s+'.$table.'\s+WHERE\s+'.$c_col.'\s*=\s*OLD\.'.$p_col.'/msi',$bdr->sql)) {
+					$on_delete = 'cascade';
+					break;
+				}
+				
+				if (preg_match('/UPDATE\s+'.$table.'\s+SET\s+'.$c_col.'\s*=\s*NULL'.
+					'\s+WHERE\s+'.$c_col.'\s*=\s*OLD\.'.$p_col.'/msi',$bdr->sql)) {
+					$on_update = 'set null';
+					break;
+				}
+			}
+			
+			$res[] = array(
+				'name' => substr($bir->name,4),
+				'c_cols' => array($c_col),
+				'p_table' => $p_table,
+				'p_cols' => array($p_col),
+				'update' => $on_update,
+				'delete' => $on_delete
+			);
+		}
+		
+		return $res;
 	}
 	
 	public function db_create_table($name,$fields)
@@ -161,7 +332,24 @@ class sqliteSchema extends dbSchema implements i_dbSchema
 	
 	public function db_create_field($table,$name,$type,$len,$null,$default)
 	{
-		# Don't need this with SQLite
+		$type = $this->udt2dbt($type,$len,$default);
+		$len = $len > 0 ? '('.$len.')' : '';
+		$null = $null ? 'NULL' : 'NOT NULL';
+		
+		if ($default === null) {
+			$default = 'DEFAULT NULL';
+		} elseif ($default !== false) {
+			$default = 'DEFAULT '.$default.' ';
+		} else {
+			$default = '';
+		}
+		
+		$sql =
+		'ALTER TABLE '.$this->con->escapeSystem($table).' '.
+		'ADD COLUMN '.$this->con->escapeSystem($name).' '.
+		$type.$len.' '.$null.' '.$default;
+		
+		$this->con->execute($sql);
 	}
 	
 	public function db_create_primary($table,$name,$cols)
@@ -241,7 +429,7 @@ class sqliteSchema extends dbSchema implements i_dbSchema
 		else # default on restrict
 		{
 			$this->x_stack[] =
-			'CREATE TRIGGER bur_'.$name."\n".
+			'CREATE TRIGGER burp_'.$name."\n".
 			'BEFORE UPDATE ON '.$p_table."\n".
 			"FOR EACH ROW BEGIN\n".
 			'  SELECT RAISE (ROLLBACK,\'update on table "'.$p_table.'" violates foreign key constraint "'.$name.'"\')'."\n".
@@ -265,7 +453,7 @@ class sqliteSchema extends dbSchema implements i_dbSchema
 			'CREATE TRIGGER bdr_'.$name."\n".
 			'BEFORE DELETE ON '.$p_table."\n".
 			"FOR EACH ROW BEGIN\n".
-			'  UPDATE '.$c_table.' SET '.$c_col .' = NULL WHERE '.$c_col.' = OLD.'.$p_col.";\n".
+			'  UPDATE '.$c_table.' SET '.$c_col.' = NULL WHERE '.$c_col.' = OLD.'.$p_col.";\n".
 			"END;\n";
 		}
 		else
@@ -282,27 +470,38 @@ class sqliteSchema extends dbSchema implements i_dbSchema
 	
 	public function db_alter_field($table,$name,$type,$len,$null,$default)
 	{
-		# Don't need this with SQLite
+		$type = $this->udt2dbt($type,$len,$default);
+		if ($type != 'integer' && $type != 'text' && $type != 'timestamp') {
+			throw new Exception('SQLite fields cannot be changed.');
+		}
 	}
 	
 	public function db_alter_primary($table,$name,$newname,$cols)
 	{
-		# Don't need this with SQLite
+		throw new Exception('SQLite primary key cannot be changed.');
 	}
 	
 	public function db_alter_unique($table,$name,$newname,$cols)
 	{
-		# Don't need this with SQLite
+		throw new Exception('SQLite unique index cannot be changed.');
 	}
 	
 	public function db_alter_index($table,$name,$newname,$type,$cols)
 	{
-		# Don't need this with SQLite
+		$this->con->execute('DROP INDEX IF EXISTS '.$name);
+		$this->con->execute('CREATE INDEX '.$newname.' ON '.$table.' ('.implode(',',$cols).') ');
 	}
 	
 	public function db_alter_reference($name,$newname,$c_table,$c_cols,$p_table,$p_cols,$update,$delete)
 	{
-		# Don't need this with SQLite
+		$this->con->execute('DROP TRIGGER IF EXISTS bur_'.$name);
+		$this->con->execute('DROP TRIGGER IF EXISTS burp_'.$name);
+		$this->con->execute('DROP TRIGGER IF EXISTS bir_'.$name);
+		$this->con->execute('DROP TRIGGER IF EXISTS aur_'.$name);
+		$this->con->execute('DROP TRIGGER IF EXISTS bdr_'.$name);
+		
+		$this->table_hist[$c_table] = $this->db_get_columns($c_table);
+		$this->db_create_reference($newname,$c_table,$c_cols,$p_table,$p_cols,$update,$delete);
 	}
 }
 ?>
