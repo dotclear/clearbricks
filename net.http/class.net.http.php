9bad1a932699fe187c9ccae7ba099c55d6b309c2
olivier@edc8846e-aa25-0410-8aa3-9560d1ba59f0 <olivier@edc8846e-aa25-0410-8aa3-9560d1ba59f0>
2008-07-21 13:16:15 +0000

    Basic scheme instead of BASIC.

--- net.http/class.net.http.php
+++ net.http/class.net.http.php
@@ -390,7 +390,7 @@ class netHttp extends netSocket
 		
 		# Basic authentication
 		if ($this->username && $this->password) {
-			$headers[] = 'Authorization: BASIC '.base64_encode($this->username.':'.$this->password);
+			$headers[] = 'Authorization: Basic '.base64_encode($this->username.':'.$this->password);
 		}
 		
 		# If this is a POST, set the content type and length
