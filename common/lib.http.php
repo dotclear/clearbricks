d574fc2fb2098f183b1ce6262a18d915a63d04b4
Dsls <none@none>
2013-11-08 22:03:46 +0100

    Removed HTTP_ACCEPT_ENCODING in browser UUID to avoid a bug for several hosters

--- common/lib.http.php
+++ common/lib.http.php
@@ -166,7 +166,6 @@ class http
 	{
 		$uid  = '';
 		$uid .= isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
-		$uid .= isset($_SERVER['HTTP_ACCEPT_ENCODING']) ? $_SERVER['HTTP_ACCEPT_ENCODING'] : '';
 		$uid .= isset($_SERVER['HTTP_ACCEPT_CHARSET']) ? $_SERVER['HTTP_ACCEPT_CHARSET'] : '';
 		
 		return crypt::hmac($key,$uid);
