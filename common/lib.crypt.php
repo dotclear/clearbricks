63bdabf4b31c7fcdb4d956d420f37dd833497e09
franck <carnet.franck.paul@gmail.com>
2016-03-22 13:28:53 +0100

    Allow other crypt alogorithms (rather than sha1 or md5), addresses #1922

--- common/lib.crypt.php
+++ common/lib.crypt.php
@@ -22,36 +22,39 @@ class crypt
 	* SHA1 or MD5 + HMAC
 	*
 	* Returns an HMAC encoded value of <var>$data</var>, using the said <var>$key</var>
-	* and <var>$hashfunc</var> as hash method (sha1 or md5 are accepted.)
+	* and <var>$hashfunc</var> as hash method (sha1 or md5 are accepted if hash_hmac function not exists.)
 	*
-	* @param	string	$key			Hash key
+	* @param	string	$key		Hash key
 	* @param	string	$data		Data
-	* @param	string	$hashfunc		Hash function (md5 or sha1)
+	* @param	string	$hashfunc	Hash function (md5 or sha1)
 	* @return string
 	*/
 	public static function hmac($key,$data,$hashfunc='sha1')
 	{
-		if ($hashfunc != 'sha1') {
-			$hashfunc = 'md5';
-		}
-		
+
 		if (function_exists('hash_hmac'))
 		{
+			if (!in_array($hasfunc,hash_algos())) {
+				$hashfunc = 'sha1';
+			}
 			return hash_hmac($hashfunc,$data,$key);
 		}
 
+		// Legacy way
+		if ($hashfunc != 'sha1') {
+			$hashfunc = 'md5';
+		}
 		$blocksize=64;
 		if (strlen($key)>$blocksize) {
 			$key=pack('H*', $hashfunc($key));
 		}
-		
 		$key=str_pad($key,$blocksize,chr(0x00));
 		$ipad=str_repeat(chr(0x36),$blocksize);
 		$opad=str_repeat(chr(0x5c),$blocksize);
 		$hmac = pack('H*',$hashfunc(($key^$opad).pack('H*',$hashfunc(($key^$ipad).$data))));
 		return bin2hex($hmac);
 	}
-	
+
 	/**
 	* Password generator
 	*
@@ -66,17 +69,16 @@ class crypt
 		$pwd = array();
 		$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
 		$chars2 = '$!@';
-		
+
 		foreach (range(0,7) as $i) {
 			$pwd[] = $chars[rand(0,strlen($chars)-1)];
 		}
-		
+
 		$pos1 = array_rand(array(0,1,2,3));
 		$pos2 = array_rand(array(4,5,6,7));
 		$pwd[$pos1] = $chars2[rand(0,strlen($chars2)-1)];
 		$pwd[$pos2] = $chars2[rand(0,strlen($chars2)-1)];
-		
+
 		return implode('',$pwd);
 	}
 }
-?>
