c28b4abb6269fc7bdbfa6554c4c686e47d7af949
franck <carnet.franck.paul@gmail.com>
2016-09-23 05:14:34 +0200

    # HG changeset patch # User Gvx <gvx@wanadoo.fr> # Date 1474315046 -7200 #      Mon Sep 19 21:57:26 2016 +0200 # Branch Ticket 2198 # Node ID 9a8e32c16b87b918ad8fb9d0d454cc462b5919c3 # Parent  56acbed367b89f91dab897d767a9f34dcab10291
    Better this way

--- dblayer/class.mysql.php
+++ dblayer/class.mysql.php
@@ -239,18 +239,16 @@ if (class_exists('dbLayer'))
 		/** @ignore */
 		public function orderBy()
 		{
+			$default = array(
+				'order'		=> '',
+				'collate'	=> false
+			);
 			foreach (func_get_args() as $v) {
 				if(is_string($v)) {
 					$res[] = $v;
-				} elseif(is_array($v) && !empty($v)) {
-					if(isset($v['order'])) {
-						$v['order'] = strtoupper($v['order']);
-						if($v['order'] != 'ASC' && $v['order'] != 'DESC') {
-							$v['order'] = '';
-						}
-					} else {
-						$v['order'] = '';
-					}
+				} elseif(is_array($v) && !empty($v['field'])) {
+					$v = array_merge($default, $v);
+					$v['order'] = (strtoupper($v['order']) == 'DESC' ?: '');
 					$res[] = $v['field'].($v['collate'] ? ' COLLATE utf8_unicode_ci' : '').' '.$v['order'];
 				}
 			}
