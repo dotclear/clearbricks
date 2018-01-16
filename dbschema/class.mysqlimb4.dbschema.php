f1fd748fa6a0fb941a96560194d4901c78a1df27
franck <carnet.franck.paul@gmail.com>
2018-01-16 14:30:01 +0100

    Fix dbschema unit tests, add mysqlimb4 tests

--- dbschema/class.mysqlimb4.dbschema.php
+++ dbschema/class.mysqlimb4.dbschema.php
@@ -13,42 +13,41 @@
 #
 # -- END LICENSE BLOCK -----------------------------------------
 
-require_once('class.mysql.dbschema.php');
+require_once 'class.mysql.dbschema.php';
 class mysqlimb4Schema extends mysqlSchema
 {
-		public function db_create_table($name,$fields)
-		{
-			$a = array();
+    public function db_create_table($name, $fields)
+    {
+        $a = array();
 
-			foreach ($fields as $n => $f)
-			{
-				$type = $f['type'];
-				$len = (integer) $f['len'];
-				$default = $f['default'];
-				$null = $f['null'];
+        foreach ($fields as $n => $f) {
+            $type    = $f['type'];
+            $len     = (integer) $f['len'];
+            $default = $f['default'];
+            $null    = $f['null'];
 
-				$type = $this->udt2dbt($type,$len,$default);
-				$len = $len > 0 ? '('.$len.')' : '';
-				$null = $null ? 'NULL' : 'NOT NULL';
+            $type = $this->udt2dbt($type, $len, $default);
+            $len  = $len > 0 ? '(' . $len . ')' : '';
+            $null = $null ? 'NULL' : 'NOT NULL';
 
-				if ($default === null) {
-					$default = 'DEFAULT NULL';
-				} elseif ($default !== false) {
-					$default = 'DEFAULT '.$default.' ';
-				} else {
-					$default = '';
-				}
+            if ($default === null) {
+                $default = 'DEFAULT NULL';
+            } elseif ($default !== false) {
+                $default = 'DEFAULT ' . $default . ' ';
+            } else {
+                $default = '';
+            }
 
-				$a[] =
-				$this->con->escapeSystem($n).' '.
-				$type.$len.' '.$null.' '.$default;
-			}
+            $a[] =
+            $this->con->escapeSystem($n) . ' ' .
+                $type . $len . ' ' . $null . ' ' . $default;
+        }
 
-			$sql =
-			'CREATE TABLE '.$this->con->escapeSystem($name)." (\n".
-				implode(",\n",$a).
-			"\n) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci";
+        $sql =
+        'CREATE TABLE ' . $this->con->escapeSystem($name) . " (\n" .
+        implode(",\n", $a) .
+            "\n) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci";
 
-			$this->con->execute($sql);
-		}
+        $this->con->execute($sql);
+    }
 }
