9d05927b01d1e125307709c81f2d28a5fb4ef60d
franck <carnet.franck.paul@gmail.com>
2018-04-28 12:28:24 +0200

    Add SQL syntax protected property for dblayer (mysql, postgresql, sqlite, …)

--- dblayer/class.mysqlimb4.php
+++ dblayer/class.mysqlimb4.php
@@ -21,6 +21,7 @@ if (class_exists('dbLayer')) {
         public static $weak_locks = true; ///< boolean: Enables weak locks if true
 
         protected $__driver = 'mysqlimb4';
+        protected $__syntax = 'mysql';
 
         public function db_connect($host, $user, $password, $database)
         {
