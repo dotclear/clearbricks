7519bc407630d6ecbb753a514c4a45e94b771a34
franck <carnet.franck.paul@gmail.com>
2019-05-17 12:18:12 +0200

    Add WebP support

--- common/lib.files.php
+++ common/lib.files.php
@@ -74,6 +74,7 @@ class files
         'svg'  => 'image/svg+xml',
         'tiff' => 'image/tiff',
         'tif'  => 'image/tiff',
+        'webp' => 'image/webp',
         'xbm'  => 'image/x-xbitmap',
 
         'css'  => 'text/css',
