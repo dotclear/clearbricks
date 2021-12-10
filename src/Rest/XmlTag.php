2605f5a27ab54dcc291b08e34944ff5b3fbde6f8
Jean-Christian Denis <contact@jcdenis.fr>
2021-12-10 22:34:24 +0100

    move rest server

--- src/Rest/XmlTag.php
+++ src/Rest/XmlTag.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @class restServer
+ * @class XmlTag
  * @brief REST Server
  *
- * A very simple REST server implementation
+ * XML Tree
  *
  * @package Clearbricks
  * @subpackage Rest
@@ -11,116 +11,9 @@
  * @copyright Olivier Meunier & Association Dotclear
  * @copyright GPL-2.0-only
  */
-class restServer
-{
-    public $rsp;                 ///< xmlTag Server response
-    public $functions = []; ///< array Server's functions
+namespace Clearbricks\Rest;
 
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->rsp = new xmlTag('rsp');
-    }
-
-    /**
-     * Add Function
-     *
-     * This adds a new function to the server. <var>$callback</var> should be
-     * a valid PHP callback. Callback function takes two arguments: GET and
-     * POST values.
-     *
-     * @param string    $name        Function name
-     * @param callable  $callback        Callback function
-     */
-    public function addFunction($name, $callback)
-    {
-        if (is_callable($callback)) {
-            $this->functions[$name] = $callback;
-        }
-    }
-
-    /**
-     * Call Function
-     *
-     * This method calls callback named <var>$name</var>.
-     *
-     * @param string    $name        Function name
-     * @param array        $get            GET values
-     * @param array        $post        POST values
-     * @return mixed
-     */
-    protected function callFunction($name, $get, $post)
-    {
-        if (isset($this->functions[$name])) {
-            return call_user_func($this->functions[$name], $get, $post);
-        }
-    }
-
-    /**
-     * Main server
-     *
-     * This method creates the main server.
-     *
-     * @param string    $encoding        Server charset
-     */
-    public function serve($encoding = 'UTF-8')
-    {
-        $get  = $_GET ?: [];
-        $post = $_POST ?: [];
-
-        if (!isset($_REQUEST['f'])) {
-            $this->rsp->status = 'failed';
-            $this->rsp->message('No function given');
-            $this->getXML($encoding);
-
-            return false;
-        }
-
-        if (!isset($this->functions[$_REQUEST['f']])) {
-            $this->rsp->status = 'failed';
-            $this->rsp->message('Function does not exist');
-            $this->getXML($encoding);
-
-            return false;
-        }
-
-        try {
-            $res = $this->callFunction($_REQUEST['f'], $get, $post);
-        } catch (Exception $e) {
-            $this->rsp->status = 'failed';
-            $this->rsp->message($e->getMessage());
-            $this->getXML($encoding);
-
-            return false;
-        }
-
-        $this->rsp->status = 'ok';
-
-        $this->rsp->insertNode($res);
-
-        $this->getXML($encoding);
-
-        return true;
-    }
-
-    private function getXML($encoding = 'UTF-8')
-    {
-        header('Content-Type: text/xml; charset=' . $encoding);
-        echo $this->rsp->toXML(1, $encoding);
-    }
-}
-
-/**
- * @class xmlTag
- *
- * XML Tree
- *
- * @package Clearbricks
- * @subpackage XML
- */
-class xmlTag
+class XmlTag
 {
     private $_name;
     private $_attr  = [];
@@ -272,3 +165,6 @@ class xmlTag
         return $res;
     }
 }
+
+/** Backwards compatibility */
+class_alias('Clearbricks\Rest\XmlTag', 'xmlTag');
