479a10472cdcc44fb557912d3c1580c3a985f5fa
Jean-Christian Denis <contact@jcdenis.fr>
2021-12-11 18:00:11 +0100

    keep things inside 'src' directory

--- _common.php
+++ _common.php
@@ -24,26 +24,10 @@
  * @copyright GPL-2.0-only
  * @version 1.0
  */
+declare(strict_types=1);
 
-namespace Clearbricks;
+require dirname(__FILE__) . '/src/Common/_main.php';
 
-define('CLEARBRICKS_VERSION', '1.2');
-
-// Define CLEARBRICKS_NO_AUTOLOAD as True to use your own autoloader
-if (!defined('CLEARBRICKS_NO_AUTOLOAD') || CLEARBRICKS_NO_AUTOLOAD !== true) {
-    require dirname(__FILE__) . '/src/Common/Autoloader.php';
-
-    $clearbricks_autoloader = new Common\Autoloader();
-    $clearbricks_autoloader->addNamespace('Clearbricks', dirname(__FILE__) . '/src');
-}
-
-// By default Keep compatibility with clearbricks < 2.0
-if (!defined('CLEARBRICKS_NO_COMPAT') || CLEARBRICKS_NO_COMPAT !== true) {
+if (CLEARBRICKS_LEGACY === true) {
     require dirname(__FILE__) . '/_legacy.php';
 }
-
-# We only need l10n __() function
-require_once dirname(__FILE__) . '/src/Common/L10n.php';
-
-# We set default timezone to avoid warning
-Common\Dt::setTZ('UTC');
---
+++ src/Common/_main.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @package Clearbricks
+ * @subpackage Common
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+declare(strict_types=1);
+
+define('CLEARBRICKS_VERSION', '1.2');
+
+# By default Keep compatibility with clearbricks < 2.0
+if (!defined('CLEARBRICKS_LEGACY')) {
+    define('CLEARBRICKS_LEGACY', true);
+}
+
+# Define CLEARBRICKS_AUTOLOAD as false to use your own autoloader
+if (!defined('CLEARBRICKS_AUTOLOAD')) {
+    define('CLEARBRICKS_AUTOLOAD', true);
+}
+
+# Legacy common class
+if (CLEARBRICKS_LEGACY === true) {
+    $__autoload = [
+        'l10n'              => dirname(__FILE__) . '/L10n.php',
+        'crypt'             => dirname(__FILE__) . '/Crypt.php',
+        'dt'                => dirname(__FILE__) . '/Dt.php',
+        'files'             => dirname(__FILE__) . '/Files.php',
+        'path'              => dirname(__FILE__) . '/Path.php',
+        'form'              => dirname(__FILE__) . '/Form.php',
+        'formSelectOption'  => dirname(__FILE__) . '/FormSelectOption.php',
+        'forms'             => dirname(__FILE__) . '/Forms.php',
+        'formsSelectOption' => dirname(__FILE__) . '/FormsSelectOption.php',
+        'html'              => dirname(__FILE__) . '/Html.php',
+        'http'              => dirname(__FILE__) . '/Http.php',
+        'text'              => dirname(__FILE__) . '/Text.php',
+    ];
+}
+
+# Instanciate Clearbricks PSR-4 autoloader
+if (CLEARBRICKS_AUTOLOAD === true) {
+    require_once dirname(__FILE__) . '/Autoloader.php';
+
+    $clearbricks_autoloader = new Clearbricks\Common\Autoloader();
+    $clearbricks_autoloader->addNamespace('Clearbricks', dirname(__FILE__) . '/../');
+}
+
+# We only need l10n __() function
+require_once dirname(__FILE__) . '/L10n.php';
+
+# We set default timezone to avoid warning
+Clearbricks\Common\Dt::setTZ('UTC');
+
+# Legacy autoloader stack
+if (!isset($__autoload) || !is_array($__autoload)) {
+    $__autoload = [];
+}
+
+# We always offer Legacy autoloader
+function cb_autoload($name)
+{
+    global $__autoload;
+
+    if (isset($__autoload[$name])) {
+        require_once $__autoload[$name];
+    }
+}
+spl_autoload_register('cb_autoload');
